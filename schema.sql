781e7e99c363a9fed4097d93e829380c63b717e2
ibugithub <outsideworkibrahim@gmail.com>
2023-07-18 14:13:22 +0600

    Modify the table creation syntax

--- schema.sql
+++ schema.sql
@@ -1,12 +1,7 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
-);
-
 CREATE TABLE animals ( 
-    id SERIAL PRIMARY KEY, 
-    name VARCHAR, 
+    id integer NOT NULL SERIAL PRIMARY KEY, 
+    name VARCHAR(50), 
     date_of_birth DATE, 
     escape_attempts INTEGER, 
     neutered BOOLEAN, 
