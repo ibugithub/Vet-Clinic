fe697866713022f7088dc360d9869f438c54cba3
Gardimy Charles <gardimycharles889@gmail.com>
2023-07-25 18:19:46 -0400

    UPDATE shema_based_on_diagram

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,56 +1,53 @@
--- Create the animals table
-CREATE TABLE animals ( 
-    id SERIAL PRIMARY KEY NOT NULL, 
-    name VARCHAR(50), 
-    date_of_birth DATE, 
-    escape_attempts INTEGER, 
-    neutered BOOLEAN, 
-    weight_kg DECIMAL(10, 2),
-    species_id INTEGER REFERENCES species(id),
-    owner_id INTEGER REFERENCES owners(id)
+/* Database schema to keep the structure of entire database. */
+
+CREATE TABLE animals (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(12,2),
+    species_id INT,
+    owner_id INT,
+    FOREIGN KEY (species_id) REFERENCES species(id),
+    FOREIGN KEY (owner_id) REFERENCES owners(id)
 );
 
--- Create the owners table
 CREATE TABLE owners (
-    id SERIAL PRIMARY KEY NOT NULL,
-    full_name VARCHAR(100),
-    age INTEGER,
-    email VARCHAR(120)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  full_name VARCHAR(100) NOT NULL,
+  age INT
 );
 
--- Create the species table
 CREATE TABLE species (
-    id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(100)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
 );
 
--- Create the vets table
 CREATE TABLE vets (
-    id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(50),
-    age INTEGER,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL ,
+    age INT,
     date_of_graduation DATE
 );
 
--- Create the specializations table
 CREATE TABLE specializations (
-    id SERIAL PRIMARY KEY,
-    vet_id INTEGER REFERENCES vets(id),
-    species_id INTEGER REFERENCES species(id)
+    vet_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id),
+    PRIMARY KEY (vet_id, species_id)
 );
 
--- Create the visits table
 CREATE TABLE visits (
-    id SERIAL PRIMARY KEY,
-    animal_id INTEGER REFERENCES animals(id),
-    vet_id INTEGER REFERENCES vets(id),
-    date_of_visit DATE
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    visit_date DATE,
+    PRIMARY KEY (animal_id, vet_id, visit_date)
 );
 
--- Add FK indexes
-CREATE INDEX ON animals (species_id);
-CREATE INDEX ON animals (owner_id);
-CREATE INDEX ON visits (animal_id);
-CREATE INDEX ON visits (vet_id);
-CREATE INDEX ON specializations (vet_id);
-CREATE INDEX ON specializations (species_id);
+-- Add indexes to foreign keys
+CREATE INDEX idx_animals_species_id ON animals (species_id);
+CREATE INDEX idx_animals_owner_id ON animals (owner_id);
+CREATE INDEX idx_vet_specializations_vet_id ON vet_specializations (vet_id);
+CREATE INDEX idx_vet_specializations_species_id ON vet_specializations (species_id);
+CREATE INDEX idx_visits_animal_id ON visits (animal_id);
+CREATE INDEX idx_visits_vet_id ON visits (vet_id);
